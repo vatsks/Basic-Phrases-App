dc92d3e7a87626a7eed20db07c90180625e7c2a4
vatsks <iamksvats@gmail.com>
2021-06-23 04:10:29 +0530

    Added tag and tag to be used to play sound files. this, getResources().getIdentifier(nameOfFile, "raw", getPackageName()).

--- app/src/main/java/com/example/basicphrasesapp/MainActivity.java
+++ app/src/main/java/com/example/basicphrasesapp/MainActivity.java
@@ -2,10 +2,21 @@ package com.example.basicphrasesapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    public  void click(View view){
 
+        Button button=(Button) view;
+        Log.i("info",button.getTag().toString());
+        MediaPlayer mediaPlayer= MediaPlayer.create( this, getResources().getIdentifier(button.getTag().toString(), "raw", getPackageName()));
+        mediaPlayer.start();
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
